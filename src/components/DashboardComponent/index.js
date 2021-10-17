b621c3df1c52c36ae3dd064fe08ae492b3c1a989
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-17 20:39:56 -0300

    :recycle: alterando estrutura dashboard

--- src/components/DashboardComponent/index.js
+++ src/components/DashboardComponent/index.js
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from 'react'
 import BreadCrumbComponent from '../breadCrumbComponent'
 import iconAccount from '../../assets/svg/account_circle-1.svg'
 import InputComponent from '../inputComponent'
-import Avatar from '../../assets/images/avatar1.png'
+import Avatar from '../../assets/images/avatar2.png'
+import Avatar1 from '../../assets/images/avatar1.png'
 import IconAccountDetail from '../../assets/svg/launch.svg'
 import ButtonComponent from '../ButtonComponent'
 import environment from '../../environment'
-import axios from '../../utils/axios'
 import LoadingComponent from '../LoadingComponent'
 
 import './styles.css'
@@ -17,21 +17,40 @@ const DashboardComponent = () => {
         page: 'Clientes'
     }
     const [loading, setLoading] = useState(true)
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState()
+
 
     useEffect(() => {
-        let options = {
-            headers: {
-                'Authorization': 'Bearer ' + environment.token
+
+        async function getData() {
+            try {
+                const url = `${environment.baseURL}/client/list`
+                const option = {
+                    method: 'GET',
+                    headers: {
+                        'Authorization': 'Bearer ' + environment.token,
+
+                    }
+                }
+                const result = await fetch(url, option)
+                const data = await result.json()
+
+                console.log(data, result)
+                setUser(data)
+
+
+                setLoading(false)
+            } catch (e) {
+                setLoading(false)
+                console.error(e)
             }
         }
 
-        const result = axios.get('/client/list', options).then(response => response).catch(e => console.log(e))
-        setUser(result)
-        console.log(result)
-        setLoading(false)
-
+        getData()
     }, [])
+
+
+
 
 
     return (
@@ -45,20 +64,27 @@ const DashboardComponent = () => {
                     <h4>Filtrar por:</h4>
                     <div className="input-area">
                         <InputComponent type="text" label="Nome" />
+                        <InputComponent type="text" label="Empresa" />
+                    </div>
+                    <div className="input-area">
+                        <InputComponent type="text" label="Situação" />
+                        <InputComponent type="text" label="Email" />
                     </div>
                 </div>
                 <div className="dashboard-body">
                     <table>
                         <thead>
-                            <th></th>
-                            <th>Clientes</th>
-                            <th>Email</th>
-                            <th>Situação</th>
-                            <th>Data de Criação</th>
-                            <th>Atualizado em</th>
-                            <th>Pagamento</th>
-                            <th>Metodo</th>
-                            <th>...</th>
+                            <tr>
+                                <th></th>
+                                <th>Clientes</th>
+                                <th>Email</th>
+                                <th>Situação</th>
+                                <th>Data de Criação</th>
+                                <th>Atualizado em</th>
+                                <th>Pagamento</th>
+                                <th>Metodo</th>
+                                <th>...</th>
+                            </tr>
                         </thead>
                         <tbody>
 
@@ -72,20 +98,21 @@ const DashboardComponent = () => {
                                 <td>
                                     <p>arthurnmrocha@gmail.com</p>
                                 </td>
-                                <td>
-                                    <p>Atrasado</p>
+                                <td className="status-area">
+                                    <span className="circle-status-yellow"></span>
+                                    <p>Pendente</p>
                                 </td>
                                 <td>
-                                    <p>09/10/2021 01:10:45</p>
+                                    <p>2021/10/14 18:17:02</p>
                                 </td>
                                 <td>
-                                    <p>09/10/2021 01:10:45</p>
+                                    <p>2021/10/14 18:17:02</p>
                                 </td>
                                 <td>
                                     <p>R$1.872,02</p>
                                 </td>
                                 <td>
-                                    <p>Crédito</p>
+                                    <p>Dinheiro</p>
                                 </td>
                                 <td>
 
@@ -94,6 +121,239 @@ const DashboardComponent = () => {
                                     </ButtonComponent>
                                 </td>
                             </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar1} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-green"></span>
+                                    <p>Recebido</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-red"></span>
+                                    <p>Atrasado</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar1} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-green"></span>
+                                    <p>Recebido</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-red"></span>
+                                    <p>Atrasado</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar1} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-yellow"></span>
+                                    <p>Pendente</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-red"></span>
+                                    <p>Atrasado</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td className="image-area">
+                                    <img src={Avatar1} alt="" />
+                                </td>
+                                <td>
+                                    <p>Arthur Rocha</p>
+                                </td>
+                                <td>
+                                    <p>arthurnmrocha@gmail.com</p>
+                                </td>
+                                <td className="status-area">
+                                    <span className="circle-status-green"></span>
+                                    <p>Recebido</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>2021/10/14 18:17:02</p>
+                                </td>
+                                <td>
+                                    <p>R$1.872,02</p>
+                                </td>
+                                <td>
+                                    <p>Dinheiro</p>
+                                </td>
+                                <td>
+
+                                    <ButtonComponent type="submit" btnDetail="true">
+                                        {<object type="image/svg+xml" data={IconAccountDetail}></object>}
+                                    </ButtonComponent>
+                                </td>
+                            </tr>
+
+
                         </tbody>
                     </table>
                 </div>
