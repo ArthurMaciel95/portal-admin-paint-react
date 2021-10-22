b6a2352a3c5314a790bf8c7fa60d59b47f76844a
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-22 00:30:40 -0300

    :sparkles: adicionando botao disabled

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
 
 import './styles.css'
-const ButtonComponent = ({ type, value = undefined, btnDetail = false, children }) => {
+const ButtonComponent = ({ type, value = undefined, btnDetail = false, children, disabled }) => {
 
 
     return (
 
-        <button className={btnDetail ? 'btn-detail' : 'btn'} type={type}>
+        <button className={btnDetail ? 'btn-detail' : 'btn'} type={type} disabled={disabled}>
             {value || children}
         </button>
 
