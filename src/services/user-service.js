bf68dc8b15ec735313c6552e81dad5f7eb14ad86
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-22 00:29:04 -0300

    :recycle: alterando regra de negocio de services

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,13 +1,22 @@
 import environment from "../environment"
 import jwt from '../utils/jwt'
-
+import buffer from "../utils/buffer";
 export const user = {
-    login: async (payload) => {
+    /**
+     * @param {string} email 
+     * @param {string} password 
+     * @returns {promise} retorna uma promise
+     * 
+     */
+    login: async (email, password) => {
+        const payload = `${email}:${password}`
+        const encoded = buffer.encoded(payload, 'base64');
+
         return await fetch(`${environment.baseURL}/user/access`, {
             method: 'GET',
             headers: {
                 'Content-Type': 'application/json',
-                'Authorization': 'Basic ' + payload
+                'Authorization': 'Basic ' + encoded
             },
         })
     },
