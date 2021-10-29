ca96e18698d1cea20c81c6f3da390d9e941ac080
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-28 23:12:23 -0300

    :sparkles: criando esqueleto para tabela quando carregar.

---
+++ src/components/skeletonTable/index.js
@@ -0,0 +1,240 @@
+import React from 'react'
+import './styles.css'
+const SkeletonTable = () => {
+    return (
+        <>
+            <tr>
+                <td>
+
+                    <div className="skeleton-image"></div>
+
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td
+                >
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                    <div className="skeleton-image"></div>
+
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td
+                >
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                    <div className="skeleton-image"></div>
+
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td
+                >
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                    <div className="skeleton-image"></div>
+
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td
+                >
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+
+                </td>
+            </tr>
+            <tr>
+                <td>
+
+                    <div className="skeleton-image"></div>
+
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td
+                >
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+                    <span className="skeleton-area">
+
+                    </span>
+                </td>
+                <td>
+
+                </td>
+            </tr>
+        </>
+    )
+}
+
+export default SkeletonTable
