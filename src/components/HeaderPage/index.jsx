34a6f1f5228d5f76d26eb643ba825887a3b8b742
ArthurMaciel95 <arthurnmrocha@gmail.com>
2021-10-30 03:53:14 -0300

    :art: introduzindo usuario logado no headercomponent

--- src/components/HeaderPage/index.jsx
+++ src/components/HeaderPage/index.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import AvatarUser from "../../assets/images/avatar1.png";
 import BreadCrumbComponent from '../BreadCrumb'
 import iconAccount from '../../assets/svg/account_circle-1.svg'
 import iconAddClient from '../../assets/svg/bread_add_list.svg'
+import jwtVerify from '../../utils/jwt'
 const HeaderPageComponent = () => {
 
+    const [currentUser, setCurrentUser] = useState({})
+    const getUser = () => {
+        const user = jwtVerify.getPayloadJwt()
+        setCurrentUser(JSON.parse(user))
+
+    }
+    useEffect(() => {
+        getUser()
+    }, [])
     const Crumb = [{
         icon: iconAccount,
         page: 'Clientes'
@@ -19,10 +29,11 @@ const HeaderPageComponent = () => {
             <BreadCrumbComponent crumb={Crumb} />
 
             <div className="user-header">
-                <img src={AvatarUser} alt="" />
+                <img src={AvatarUser} alt="foto de usuário" />
                 <div className="user-description">
-                    <p>Nome User </p>
-                    <p>danieljose@gmail.com</p>
+                    {currentUser ? <><p>{currentUser.username}</p>
+                        <p>{currentUser.email}</p></> : {}}
+
                 </div>
             </div>
         </div>);
